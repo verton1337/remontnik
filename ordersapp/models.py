2cc5fa21df386be8abe3d5dd3132fdd0fa58a635
Alex <verton1337@ya.ru>
2021-10-17 13:08:52 +0300

    Changed model Order

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -27,3 +27,5 @@ class Order(models.Model):
                               default=FORMING)
     worker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     description = models.TextField(max_length=512, blank=True, verbose_name="Описание заказа")
+    created = models.DateTimeField(verbose_name='создан', auto_now_add=True, null=True)
+    updated = models.DateTimeField(verbose_name='обновлен', auto_now=True)
